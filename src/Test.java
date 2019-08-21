12655ca438577e92efd260d24013041f193ac717
Durgadas-Kamath1 <Durgadas.Kamath1@ibm.com>
2019-08-21 15:14:22 +0530

    Testing class 123456

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) throws Exception {
-        System.out.println("Testing 123 !!!!");
+        System.out.println("Testing 123456 !!!!");
     }
 }
