f61b729f779979929b3d17347b6f88d3a696c8b5
Durgadas-Kamath1 <Durgadas.Kamath1@ibm.com>
2019-08-21 15:12:43 +0530

    I am enjoying the training

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java -- We are Awesome !!!!");
-        System.out.println("I am not bored with the training...");
+        System.out.println("I am enjoying the training...");
     }
 }
