e1de3c2d887d469a75ad00818a53026aee1deac8
Durgadas-Kamath1 <Durgadas.Kamath1@ibm.com>
2019-08-21 14:34:11 +0530

    fix defect from I to We

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java -- I am Awesome !!!!");
+        System.out.println("Hello Java -- We are Awesome !!!!");
     }
 }
