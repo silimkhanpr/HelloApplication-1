7f81d95f3ab62d656cbe14cb47e27152ae3d70eb
Nitish-Yadav <nitish_yadav@persistent.com>
2019-08-22 12:05:18 +0530

    Added to forked repo

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+ 
+   public static void main(String[] args) throws Exception {
         System.out.println("Hello Java -- We are Awesome !!!!");
         System.out.println("I am enjoying the training...");
+	System.out.println("Loving git");
     }
 }
