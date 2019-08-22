75dff988d8026f79503c9e11ddbe530711051524
rushikeshprabhudesai20 <44064266+rushikeshprabhudesai20@users.noreply.github.com>
2019-08-22 12:30:36 +0530

    Rushikesh p (#14)
    * Rushikesh comment
    
    * rushikesh_p

--- src/App.java
+++ src/App.java
@@ -3,8 +3,13 @@ public class App {
    public static void main(String[] args) throws Exception {
         System.out.println("Hello Java -- We are Awesome !!!!");
         System.out.println("I am enjoying the training...");
+
+        System.out.println("this is rushikesh.");
+        //this is a comment from Rushikesh Prabhudesai.
+
      System.out.println("hii changes made by vijay");
         	System.out.println("Loving git");
+
 
     }
 }
